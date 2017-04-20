e698338864b80186fac79b2914d4ff4eab110087
Chris Swarbrick <chris.swarb@gmail.com>
2017-04-20 23:07:24 +0100

    [development] - Model experiment

--- src/app/shared/delay/delay.component.ts
+++ src/app/shared/delay/delay.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnChanges, ChangeDetectionStrategy, ChangeDetectorRef, Input, Output, HostBinding } from "@angular/core";
 import { Resolve, ActivatedRoute } from "@angular/router";
 
+import { delayModel } from "./delayModel";
+
 @Component({
 	moduleId: module.id,
     selector: "[emergency-delays]",
@@ -8,7 +10,7 @@ import { Resolve, ActivatedRoute } from "@angular/router";
 })
 export class delayComponent implements OnInit {
 
-	delays: any = [];
+	delays: delayModel[] = [];
 
 	constructor(private route: ActivatedRoute){}	
 
@@ -22,8 +24,24 @@ export class delayComponent implements OnInit {
 
 	private concatArray(delayArray: any): void {
 		delayArray.map((value, iterator) => {
-			this.delays = this.delays.concat(value);
+			if(value.length < 1) {return false};
+			console.log(value[0]["category"]);
+			this.delays = this.delays.concat(
+				new delayModel(
+					[value][0]["$type"],
+					[value][0]["category"],
+					[value][0]["type"],
+					[value][0]["categoryDescription"],
+					[value][0]["description"],
+					[value][0]["affectedRoutes"],
+					[value][0]["affectedStops"],
+					[value][0]["closureText"]
+				)
+			);
 		});
 	}
 
+	check() {
+		console.log(this);
+	}
 }
---
+++ src/app/shared/delay/delayModel.ts
@@ -0,0 +1,12 @@
+export class delayModel {
+
+	constructor(public $type: string, 
+		public category: string,
+	 	public type: string,
+		public categoryDescription: string,
+		public description: string,
+		public affectedRoutes: any,
+		public affectedStops: any, 
+		public closureText: string
+	) {}
+}
