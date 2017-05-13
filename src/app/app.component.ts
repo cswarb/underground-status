3a808e1e0a794b62561df744880a81b3fa5d0add
Chris Swarbrick <chris.swarb@gmail.com>
2017-05-13 21:02:16 +0100

    [development] - Added ability to hook into route loading to display a loading screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import { Router, Event as RouterEvent, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from "@angular/router"
 
 @Component({
     moduleId: module.id,
     selector: "[app-component]",
     templateUrl: "./app.template.html"
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	loading: boolean = false;
+
+	constructor(private router: Router) {}
+
+	ngOnInit() {
+		this.setupLoading();
+	}
+
+	private setupLoading(): void {
+		this.router.events.subscribe((event: RouterEvent) => {
+            this.navigationInterceptor(event);
+        });
+	}
+
+	private navigationInterceptor(event: RouterEvent): void {
+        if (event instanceof NavigationStart) {
+			this.loading = true;
+        } else if(event instanceof NavigationCancel || event instanceof NavigationError || event instanceof NavigationEnd) {
+			this.loading = false;
+        };
+    }
 
 }
